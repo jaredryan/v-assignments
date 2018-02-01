efab3385b3de1a289ad0d543dbb9c449b9ec5543
Jared Ryan <jryantennis@gmail.com>
2018-02-01 14:50:18 -0700

    corrected caesar cipher oversite

--- exercises/caeser-cipher/app.js
+++ exercises/caeser-cipher/app.js
@@ -13,7 +13,18 @@ console.log("Encrypted String: " + newStr);
    indicating how to the right on the alphabet it should slide. */
 function getNewChar(string, index, numToShiftRight) {
 	var curChar = string.charCodeAt(index);
-	curChar += parseInt(numToShiftRight);
-	if (curChar > 122) curChar -= 26;
-	return String.fromCharCode(curChar);
+	// Upper case alphabet is from 65-90
+	if (curChar <= 90 && curChar >= 65) {
+		curChar += parseInt(numToShiftRight);
+		if (curChar > 90) curChar -= 26;
+		return String.fromCharCode(curChar);
+	}
+	// Lower case alphabet is from 97-122
+	if (curChar <= 122 && curChar >= 97) {
+		curChar += parseInt(numToShiftRight);
+		if (curChar > 122) curChar -= 26;
+		return String.fromCharCode(curChar);
+	}
+	// If any other character, just return it
+	return string[index];
 }
