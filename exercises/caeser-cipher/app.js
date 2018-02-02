4cf848b2292ee8c8b6e342aa87e91ad546d21ea1
Jared Ryan <jryantennis@gmail.com>
2018-02-01 19:02:47 -0700

    copied from other repo--there is a link in the README to take you to the repo where it is on its own

--- exercises/caeser-cipher/app.js
+++ exercises/caeser-cipher/app.js
@@ -1,7 +1,7 @@
 // Set up
 var readlineSync = require("readline-sync");
 var string = readlineSync.question("Enter your string: ");
-var numToShiftRight = readlineSync.question("Enter the number of characters to shift right: ");
+var numToShiftRight = parseInt(readlineSync.question("Enter the number of characters to shift right: "));
 
 var newStr = ""
 for (var i = 0; i < string.length; i++) {
@@ -15,13 +15,13 @@ function getNewChar(string, index, numToShiftRight) {
 	var curChar = string.charCodeAt(index);
 	// Upper case alphabet is from 65-90
 	if (curChar <= 90 && curChar >= 65) {
-		curChar += parseInt(numToShiftRight);
+		curChar += numToShiftRight;
 		if (curChar > 90) curChar -= 26;
 		return String.fromCharCode(curChar);
 	}
 	// Lower case alphabet is from 97-122
 	if (curChar <= 122 && curChar >= 97) {
-		curChar += parseInt(numToShiftRight);
+		curChar += numToShiftRight;
 		if (curChar > 122) curChar -= 26;
 		return String.fromCharCode(curChar);
 	}
--- projects/business-time/Skills/app.js
+++ projects/business-time/Skills/app.js
@@ -1,13 +1,18 @@
-var navbar = 	'<a href="../index.html"><h1>Tech JobPrep</h1></a>' + 
-				'<ul>' + 
-					'<a href="../About/index.html"><li>About</li></a>' + 
-					'<a href="index.html"><li>Skills</li></a>' + 
-					'<a href="../Presentation/index.html"><li>Presentation</li></a>' + 
-					'<a href="../Interviewing/index.html"><li>Interviewing</li></a>' + 
-				'</ul>';
-
-
-$(function() {
-	document.getElementById("navbar").innerHTML = navbar;
- 	$("#favicon-placeholder").load("../favicon.html");
+// Open/close dropdown menu when clicked
+$(".dropbtn").on("click", function() {
+    document.getElementById("myDropdown").classList.toggle("show");
 });
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
