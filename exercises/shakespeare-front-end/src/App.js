28b494979123f9f85bfa409c61d3ca1b8835e324
Jared Ryan <jryantennis@gmail.com>
2018-04-12 15:22:56 -0600

    added buttons to sort the ratings in ascending or descending order

--- exercises/shakespeare-front-end/src/App.js
+++ exercises/shakespeare-front-end/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import axios from 'axios';
+import RaisedButton from 'material-ui/RaisedButton';
+
 axios.defaults.headers.common['Authorization'] = "koOheljmQX";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            reviews: [],
-            body: ""
+            reviews: []
         }
     }
 
@@ -29,8 +30,23 @@ class App extends Component {
         })
     }
 
+    ascending = () => {
+        this.setState(prevState => {
+            const reviews = prevState.reviews.sort((a, b) => a.rating - b.rating)
+            return {reviews}
+        })
+    }
+
+    descending = () => {
+        this.setState(prevState => {
+            const reviews = prevState.reviews.sort((a, b) => b.rating - a.rating)
+            return {reviews}
+        })
+    }
+
     renderReviews = () => {
-        return this.state.reviews.map((review, index) => {
+        return this.state.reviews
+            .map((review, index) => {
             return  (<Card key={index + review.id} style={{marginBottom: "10px"}}>
                         <CardHeader
                           title={`Rating: ${review.rating}`}
@@ -55,6 +71,16 @@ class App extends Component {
           <div className="title">
               <div className="background"></div>
               <h1>Shakespeare's Reviews</h1>
+          </div>
+          <div className="buttons">
+              <RaisedButton
+                  label="Ascending"
+                  onClick={this.ascending}
+                  style={{width: "120px", marginRight: "5px", marginLeft: "5px"}}/>
+              <RaisedButton
+                  label="Descending"
+                  onClick={this.descending}
+                  style={{width: "120px", marginRight: "5px", marginLeft: "5px"}}/>
           </div>
           <div className="reviews">
               {this.renderReviews()}
